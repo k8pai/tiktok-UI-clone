339254c5eaa773414dc42d13843c72cc31c83c4e
k8pai <thek8pai@gmail.com>
2023-01-26 14:45:11 +0530

    toggle play

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Video.css'
 import VideoFooter from './FooterLeft';
 import FooterRight from './FooterRight';
@@ -6,8 +6,27 @@ import FooterRight from './FooterRight';
 export default function Video(props) {
 	// write your code here
 	const { url, channel, description, song, likes, shares, messages } = props;
+	const videoRef = useRef(null);
+
+	const onVideoPress = () => {
+		if(videoRef.current.paused){
+			videoRef.current.play()
+		} else {
+			videoRef.current.pause()
+		}
+	}
+
+
 	return <div className='video'>
-		<video className='player' src={url} loop></video>
+		<video 
+			className='player' 
+			onClick={onVideoPress}
+			muted 
+			ref={videoRef}
+			loop
+			src={url}>
+
+		</video>
 		<div className='bottom-controls'>
 			<VideoFooter channel={channel} description={description} song={song} />
 			<FooterRight likes={likes} shares={shares} messages={messages} />
