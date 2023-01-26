3a792c9f7d5c850a4f1243314af79bb2f1eee078
k8pai <thek8pai@gmail.com>
2023-01-26 13:06:01 +0530

    loading multiple videos through api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Video from './Video'
 import './App.css'
 
+const API_URL =
+	'https://raw.githubusercontent.com/codedamn-classrooms/tiktok-react-material/main/data.json'
+
 export default function App() {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		fetch(API_URL).
+			then(data => data.json()).
+			then(data => setVideos(data));
+	}, [])
+
 	return (
 		<div className="app">
 			<div className="container">
+				{videos.map((data, ind) => {
+					return <Video url={data.url} />
+				})}
 				<Video url="https://github.com/codedamn-classrooms/tiktok-react-material/raw/main/v1.mp4" />
 			</div>
 		</div>
